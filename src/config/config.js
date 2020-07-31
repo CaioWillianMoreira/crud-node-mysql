372dada17317d4e70055aa52069dbbe3fab34f68
Caio Willian <cwillian40@hotmail.com>
2020-07-31 00:13:36 -0300

    [#02] Adicionando a string de conexão

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+// string de conexão
+module.exports = {
+  development: {
+    database: {
+      host: 'localhost',
+      port: 3306,
+      name: 'cursoNodeMySQL',
+      dialect: 'mysql',
+      user: 'root',
+      password: ''
+    }
+  },
+  production: {
+    database: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+    }
+  }
+}
